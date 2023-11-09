df23bceba22769e2f8226b71efe2de0e850a82ef
David71-star <davidcortes024@gmail.com>
2023-11-09 16:02:33 +0100

    aggiunta ; riga 123

--- domande.js
+++ domande.js
@@ -5,13 +5,18 @@ const questions = [
     difficulty: "easy",
     question: "What does CPU stand for?",
     correct_answer: "Central Processing Unit",
-    incorrect_answers: ["Central Process Unit", "Computer Personal Unit", "Central Processor Unit"],
+    incorrect_answers: [
+      "Central Process Unit",
+      "Computer Personal Unit",
+      "Central Processor Unit",
+    ],
   },
   {
     category: "Science: Computers",
     type: "multiple",
     difficulty: "easy",
-    question: "In the programming language Java, which of these keywords would you put on a variable to make sure it doesn't get modified?",
+    question:
+      "In the programming language Java, which of these keywords would you put on a variable to make sure it doesn't get modified?",
     correct_answer: "Final",
     incorrect_answers: ["Static", "Private", "Public"],
   },
@@ -27,7 +32,8 @@ const questions = [
     category: "Science: Computers",
     type: "boolean",
     difficulty: "easy",
-    question: "Pointers were not used in the original C programming language; they were added later on in C++.",
+    question:
+      "Pointers were not used in the original C programming language; they were added later on in C++.",
     correct_answer: "False",
     incorrect_answers: ["True"],
   },
@@ -35,7 +41,8 @@ const questions = [
     category: "Science: Computers",
     type: "multiple",
     difficulty: "easy",
-    question: "What is the most preferred image format used for logos in the Wikimedia database?",
+    question:
+      "What is the most preferred image format used for logos in the Wikimedia database?",
     correct_answer: ".svg",
     incorrect_answers: [".png", ".jpeg", ".gif"],
   },
@@ -45,13 +52,18 @@ const questions = [
     difficulty: "easy",
     question: "In web design, what does CSS stand for?",
     correct_answer: "Cascading Style Sheet",
-    incorrect_answers: ["Counter Strike: Source", "Corrective Style Sheet", "Computer Style Sheet"],
+    incorrect_answers: [
+      "Counter Strike: Source",
+      "Corrective Style Sheet",
+      "Computer Style Sheet",
+    ],
   },
   {
     category: "Science: Computers",
     type: "multiple",
     difficulty: "easy",
-    question: "What is the code name for the mobile operating system Android 7.0?",
+    question:
+      "What is the code name for the mobile operating system Android 7.0?",
     correct_answer: "Nougat",
     incorrect_answers: ["Ice Cream Sandwich", "Jelly Bean", "Marshmallow"],
   },
@@ -75,7 +87,8 @@ const questions = [
     category: "Science: Computers",
     type: "multiple",
     difficulty: "easy",
-    question: "Which programming language shares its name with an island in Indonesia?",
+    question:
+      "Which programming language shares its name with an island in Indonesia?",
     correct_answer: "Java",
     incorrect_answers: ["Python", "C", "Jakarta"],
   },
@@ -92,6 +105,7 @@ let indice = document.querySelector("#index");
 let rispostaSelezionata = "";
 
 let arrayRisposteUser = [];
+
 for (let i = 0; i < questions.length; i++) {
   questions[i].incorrect_answers.push(questions[i].correct_answer);
   arrayRisposteUser.push(questions[i].incorrect_answers);
@@ -106,9 +120,9 @@ const aggiungiListener = function () {
 };
 
 let finish = function () {
-  document.querySelector('footer').innerHTML= ""
-  trovaDomanda.innerText = "Il tuo punteggio finale è " + punteggioUtente
-}
+  document.querySelector("footer").innerHTML = "";
+  trovaDomanda.innerText = "Il tuo punteggio finale è " + punteggioUtente;
+};
 
 const creaBottoni = function () {
   buttonRisposta.innerHTML = "";
@@ -120,10 +134,10 @@ const creaBottoni = function () {
   aggiungiListener();
 
   console.log(questionNumber);
-  if(questionNumber === 9) {
+  if (questionNumber === 9) {
     for (let i = 0; i < buttonRisposta.children.length; i++) {
-      buttonRisposta.children[i].addEventListener("click", function() {
-        finish()
+      buttonRisposta.children[i].addEventListener("click", function () {
+        finish();
       });
     }
   }
